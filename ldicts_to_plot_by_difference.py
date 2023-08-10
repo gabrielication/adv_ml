7ad2f993e0af67953406aec36ed6180abfb0d6df
Gabriele Restuccia <gabriele.restuccia92@gmail.com>
2023-08-10 09:19:13 -0400

    new plots added

--- ldicts_to_plot_by_difference.py
+++ ldicts_to_plot_by_difference.py
@@ -44,55 +44,58 @@ for lnorm_dict in dicts_list:
             # Replace this line with the logic to fill the matrix based on your data
             matrix[i, j] = lnorm_dict[i][j][0]
 
-    # Create figure and axis
-    fig, ax = plt.subplots(figsize=(20, 20))
-
     # Set the diagonal of the matrix to 0.0
     np.fill_diagonal(matrix, 0.0)
 
-    # Plot the matrix
-    cax = ax.matshow(matrix, cmap='Blues')
-
+    # Create the labels
     row_labels = ['tiger', 'zebra', 'gorilla', 'gibbon', 'canoe', 'gondola', 'h-disk', 'sc-bus', 'tramcar', 'volcano']
     col_labels = ['tiger', 'zebra', 'gorilla', 'gibbon', 'canoe', 'gondola', 'h-disk', 'sc-bus', 'tramcar', 'volcano']
 
-    # Add grid lines
-    ax.set_xticks(np.arange(10))
-    ax.set_yticks(np.arange(10))
-    ax.set_xticklabels(col_labels, rotation=45, ha='right')
-    ax.set_yticklabels(row_labels)
+    filtered = ['gorilla','gondola']
 
-    # Add values to the matrix with higher precision
-    print("Adding values to plot %d..." % count)
-    for i in range(10):
-        for j in range(10):
-            ax.text(j, i, '{:.3e}'.format(matrix[i, j]), ha='center', va='center', color='black', fontweight='bold')
+    # col_labels = ['zebra','sc-bus']
+    # Assuming your matrix is named m
+    # m = np.array(....)
 
-    # Set colorbar
-    cbar = fig.colorbar(cax)
+    # # Add the small constant to avoid issues with log scale
+    # epsilon = 1e-10
+    # column1 += epsilon
+    # column2 += epsilon
 
-    # Set axis labels
-    plt.xlabel("Original Classes")
-    plt.ylabel("Target Classes")
+    # Calculate the percentage difference
+    # percentage_difference = (np.abs(column2 - column1) / ((column1 + column2) / 2)) * 100
+
+    plt.figure(figsize=(14, 8))
+
+    # Plotting each row
+    for idx, row_label in enumerate(row_labels):
+        if row_label in filtered:
+            row_data = matrix[idx, :]
+            plt.plot(col_labels, row_data, marker='o', label=row_label)
+
+            # Annotate points for the current row
+            for i, val in enumerate(row_data):
+                plt.text(i, val, f"{val:.5f}", ha='center', va='bottom')
 
     if count == 3:
         nstr = "inf"
     else:
         nstr = str(count)
 
-    title = "L"+nstr+" norm"
-    filename = "L"+nstr+"_matrix.png"
+    plt.title('L' + nstr + ' Perturbation Magnitudes for Different Classes')
+    plt.xlabel('Target Classes')
+    plt.ylabel('Perturbation Magnitude')
+    plt.grid(True)
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))  # Place legend outside the plot
+    plt.tight_layout()  # Adjust layout for better visualization
 
-    # Show the plot
-    plt.title(title)
+    ax = plt.gca()
+    ax.set_yscale('symlog')
 
-    # Save the plot as an image file (e.g., PNG)
-    plt.savefig(filename, dpi=300)  # Change the filename and extension as needed
+    # Set the y limit to the minimum non-zero value from the matrix
+    min_value = np.min(matrix[matrix > 0])
+    ax.set_ylim(bottom=0)
 
-    print("Plot saved as: ",filename)
+    plt.show()
 
-    count += 1
-
-    # plt.show()
-
-print()
+    count += 1  # Increment the count after processing each dictionary
